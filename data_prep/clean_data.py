231bba4e676dce363b0dcedc7c7c13b640116721
quentinseifert <50702131+quentinseifert@users.noreply.github.com>
2019-11-29 09:40:18 +0100

    updated data_prep

--- data_prep/clean_data.py
+++ data_prep/clean_data.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 
+
 # define function to remove and replace unwanted signs in files
 def clean_trees(path):
     file_list = os.listdir(path)
@@ -19,13 +20,51 @@ def clean_trees(path):
 
 # use clean trees to go through data and clean all files (actually unnecessary to go through every directory)
 
-label_list = os.listdir('../data/')
+label_list = ['Laubbäume', 'Nadelbäume']
 
 for label in label_list:
     species_list = os.listdir('../data/' + label)
     for species in species_list:
         path = os.path.join('../data', label, species)
         clean_trees(path)
+
+file_list = os.listdir('../data/laubbäume/unsortiert')
+file_list = sorted(file_list, key=lambda x: int(os.path.splitext(x)[0]))
+path = '../data/laubbäume/unsortiert'
+out = []
+for file in file_list:
+    file_path = os.path.join(path, file)
+    print(file)
+    with open(file_path, 'r') as f:
+        data = [line.rstrip() for line in f]
+    if len(data) < 2048:
+        out.append(file_path)
+        os.remove(file_path)
+        continue
+    lengths = []
+    for i in range(0, len(data)):
+        data[i] = [x for x in data[i].split()]
+        lengths.append(len(data[i]) < 3)
+
+    idx = [i for i, x in enumerate(lengths) if x]
+
+    for id in sorted(idx, reverse=True):
+        del (data[id])
+
+    for i in range(0, len(data)):
+        data[i] = [float(x) for x in data[i]]
+
+    np.savetxt(file_path, np.array(data)[:, :3])
+
+
+# save file names of files that are thrown out
+if not os.path.exists('../data/unsorted_labels'):
+    os.mkdir('../data/unsorted_labels')
+
+with open('../data/unsorted_labels/out_files.txt', 'w+') as f:
+    f.write('\n'.join(idx))
+
+
 
 
 
--- data_prep/import_files.py
+++ data_prep/import_files.py
@@ -1,6 +1,9 @@
 import numpy as np
 import os
 import random
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+import h5py
 
 
 def get_data(directory):
@@ -24,7 +27,7 @@ def get_data(directory):
                 lines[i] = [float(x) for x in lines[i].split()]
 
             lines = random.sample(lines, 2048)
-            lines = np.array(lines)[:,:3]
+            lines = np.array(lines)[:, :3]
             trees.append(lines)
 
         data = np.array(trees)
@@ -37,17 +40,25 @@ n_trees, n_counts = get_data('../data/Nadelbäume')
 l_trees, l_counts = get_data('../data/Laubbäume')
 
 
+label_n = np.repeat("Nadel", 70)
+label_l = np.repeat("Laub", 239)
+labels = np.append(label_l, label_n)
 
 
+trees = np.concatenate([l_trees, n_trees], axis=0)
 
+lab_bin = LabelBinarizer()
+labels = lab_bin.fit_transform(labels)
 
+x_train, x_test, y_train, y_test = train_test_split(trees, labels, test_size=0.3, random_state=41)
 
+if not os.path.exists('../h5_data/'):
+    os.mkdir('../h5_data/')
 
+with h5py.File('../h5_data/train.h5', 'w') as f:
+    f.create_dataset('data', data=x_train)
+    f.create_dataset('label', data=y_train)
 
-
-
-    
-
-
-
-
+with h5py.File('../h5_data/test.h5', 'w') as f:
+    f.create_dataset('data', data=x_test)
+    f.create_dataset('label', data=y_test)
