52f7594b4ca2ade12372a7dcebd10ccf9c921e48
quentinseifert <50702131+quentinseifert@users.noreply.github.com>
2019-11-30 14:21:43 +0100

    added argument parser and cutoff

--- data_prep/import_files.py
+++ data_prep/import_files.py
@@ -1,12 +1,25 @@
+import argparse
 import numpy as np
 import os
-import random
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
 import h5py
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_points', type=int, default=2048, help='number of points to be sampled from cloud')
+parser.add_argument('--k_nadel', type=int, default=1, help='number of trees to be sampled per nadel')
+parser.add_argument('--k_laub', type=int, default=1, help='number of trees to be sampled per laub')
+parser.add_argument('--cutoff', type=float, default=0.0, help='percentage of data to be cut from tree')
+FLAGS = parser.parse_args()
 
-def get_data(directory, n_points=2048, k=1):
+NUM_POINTS = FLAGS.num_points
+K_NADEL = FLAGS.k_nadel
+K_LAUB = FLAGS.k_laub
+CUTOFF = FLAGS.cutoff
+
+
+
+def get_data(directory, n_points=NUM_POINTS, k=1, cutoff=CUTOFF):
     label_list = os.listdir(directory)
     trees = []
     counts = []
@@ -16,20 +29,20 @@ def get_data(directory, n_points=2048, k=1):
         path = os.path.join(directory, label)
         file_list = os.listdir(path)
         counts.append(len(file_list) * k)
+        print(label)
 
         for file in file_list:
             file_path = os.path.join(path, file)
+            lines = np.loadtxt(file_path)[:, :3]
 
-            with open(file_path, 'r+') as f:
-                lines = [line.rstrip() for line in f]
+            if (cutoff > 0):
+                lines = lines[lines[:, 2].argsort()]
+                bound = int(cutoff * lines.shape[0])
+                lines = lines[bound:, :]
 
-            for i in range(0, len(lines)):
-                lines[i] = [float(x) for x in lines[i].split()]
-            
-            
             for i in range(k):
-                lines_new = random.sample(lines, n_points)
-                lines_new = np.array(lines_new)[:, :3]
+                idx = np.random.randint(lines.shape[0], size=n_points)
+                lines_new = lines[idx, :]
                 trees.append(lines_new)
 
         data = np.array(trees)
@@ -38,8 +51,8 @@ def get_data(directory, n_points=2048, k=1):
 
 
 # import data from files
-n_trees, n_counts = get_data('../data/Nadelbäume', n_points=4096,k=3)
-l_trees, l_counts = get_data('../data/Laubbäume', n_points=4096)
+n_trees, n_counts = get_data('../data/Nadelbäume', n_points=NUM_POINTS, k=K_NADEL)
+l_trees, l_counts = get_data('../data/Laubbäume', n_points=NUM_POINTS, k=K_LAUB)
 
 
 label_n = np.repeat("Nadel", np.sum(n_counts))
