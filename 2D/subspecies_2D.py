18ae5187c4952919440162cffe3468a86baafddd
Amtomkraftwerk <antonthielmann@t-online.de>
2020-03-03 12:43:03 +0100

    new augmentation

--- 2D/subspecies_2D.py
+++ 2D/subspecies_2D.py
@@ -2,6 +2,9 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 import argparse
 import numpy as np
+import numpy as np
+import imgaug as ia
+import imgaug.augmenters as iaa
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -15,9 +18,10 @@ from keras.models import load_model
 parser = argparse.ArgumentParser()
 parser.add_argument('--epochs', type=int, default=50, help='number of training epochs')
 parser.add_argument('--val_size', type=float, default=0.2, help='size of validation data')
-parser.add_argument('--batch_size', type=int, default=128, help='number of instances per batch')
-parser.add_argument('--kernel_size', type=int, default=(5, 5), help='kernel size for model')
-parser.add_argument('--drop', type=float, default=0.5, help='dropout prob')
+parser.add_argument('--batch_size', type=int, default=164, help='number of instances per batch')
+parser.add_argument('--kernel_size', type=int, default=(3, 3), help='kernel size for model')
+parser.add_argument('--drop', type=float, default=0.35, help='dropout prob')
+parser.add_argument('--name', type=str, default='sub_model.h5', help='specify name of saved model' )
 
 FLAGS = parser.parse_args()
 
@@ -26,6 +30,7 @@ VAL_SIZE = FLAGS.val_size
 BATCH_SIZE = FLAGS.batch_size
 KERNEL_SIZE = FLAGS.kernel_size
 DROP = FLAGS.drop
+NAME = FLAGS.name
 
 
 images_training = ImageDataGenerator().flow_from_directory('Images', target_size=(225, 150),
@@ -69,13 +74,13 @@ rotate.fit(x_small)
 vert.fit(x_small)
 hor.fit(x_small)
 
-for x_rot_s, y_rot_s in rotate.flow(x_small, y_small, batch_size=200, shuffle=True):
+for x_rot_s, y_rot_s in rotate.flow(x_small, y_small, batch_size=700, shuffle=True):
     break
 
-for x_vert_s, y_vert_s in vert.flow(x_small, y_small, batch_size=200, shuffle=True):
+for x_vert_s, y_vert_s in vert.flow(x_small, y_small, batch_size=700, shuffle=True):
     break
 
-for x_hor_s, y_hor_s in hor.flow(x_small, y_small, batch_size=200, shuffle=True):
+for x_hor_s, y_hor_s in hor.flow(x_small, y_small, batch_size=700, shuffle=True):
     break
 
 x_small = np.concatenate((x_small, x_rot_s), axis=0)
@@ -120,9 +125,102 @@ y_train = np.concatenate((y_train, y_hor), axis=0)
 x_train = x_train[:, 40:175, 20:120, :]
 x_test = x_test[:, 40:175, 20:120, :]
 
-print(x_train.shape)
-plt.imshow(x_train[1,:,:,0])
-plt.show()
+#sometimes = lambda aug: iaa.Sometimes(1.0, aug)
+#
+## Define our sequence of augmentation steps that will be applied to every image.
+#seq = iaa.Sequential(
+#    [
+#
+#        # crop some of the images by 0-10% of their height/width
+#        sometimes(iaa.Crop(percent=(0, 0.05))),
+#
+#        # Apply affine transformations to some of the images
+#        # - scale to 80-120% of image height/width (each axis independently)
+#        # - translate by -20 to +20 relative to height/width (per axis)
+#        # - rotate by -45 to +45 degrees
+#        # - shear by -16 to +16 degrees
+#        # - order: use nearest neighbour or bilinear interpolation (fast)
+#        # - mode: use any available mode to fill newly created pixels
+#        #         see API or scikit-image for which modes are available
+#        # - cval: if the mode is constant, then use a random brightness
+#        #         for the newly created pixels (e.g. sometimes black,
+#        #         sometimes white)
+#        sometimes(iaa.Affine(
+#            scale={"x": (0.995, 1.005), "y": (0.995, 1.005)},
+#            translate_percent={"x": (-0.005, 0.005), "y": (-0.005, 0.005)},
+#            order=[0, 1],
+#            #cval=(0, 255),
+#            mode=ia.ALL
+#        )),
+#
+#        #
+#        # Execute 0 to 5 of the following (less important) augmenters per
+#        # image. Don't execute all of them, as that would often be way too
+#        # strong.
+#        #
+#        iaa.SomeOf((0, 2),
+#            [
+#
+#
+#                # Blur each image with varying strength using
+#                # gaussian blur (sigma between 0 and 3.0),
+#                # average/uniform blur (kernel size between 2x2 and 7x7)
+#                # median blur (kernel size between 3x3 and 11x11).
+#                iaa.OneOf([
+#                    iaa.GaussianBlur((0, 0.005)),
+#                    iaa.AverageBlur(k=(0.1, 0.15)),
+#                ]),
+#
+#                # Sharpen each image, overlay the result with the original
+#                # image using an alpha between 0 (no sharpening) and 1
+#                # (full sharpening effect).
+#                iaa.Sharpen(alpha=(0, 0.1), lightness=(0.95, 1)),
+#
+#                # Same as sharpen, but for an embossing effect.
+#                iaa.Emboss(alpha=(0, 0.1), strength=(0, 0.1)),
+#
+#                # Improve or worsen the contrast of images.
+#                iaa.LinearContrast((0.97, 1.02), per_channel=0.01),
+#
+#
+#                # In some images distort local areas with varying strength.
+#                #sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.02)))
+#            ],
+#            # do all of the above augmentations in random order
+#            random_order=False
+#        )
+#    ],
+#    # do all of the above augmentations in random order
+#    random_order=False
+#)
+#
+#images_aug = seq(images=x_train)
+#
+#x_train = np.concatenate((x_train, images_aug), axis=0)
+#y_train = np.concatenate((y_train, y_train), axis=0)
+#
+#print(x_train.shape)
+#print(y_train.shape)
+#plt.imshow(x_train[20015,:,:,0])
+#plt.show()
+#plt.imshow(x_train[20014,:,:,0])
+#plt.show()
+#plt.imshow(x_train[20005,:,:,0])
+#plt.show()
+#plt.imshow(x_train[23015,:,:,0])
+#plt.show()
+#plt.imshow(x_train[21015,:,:,0])
+#plt.show()
+#plt.imshow(x_train[25015,:,:,0])
+#plt.show()
+#plt.imshow(x_train[24015,:,:,0])
+#plt.show()
+#plt.imshow(x_train[24515,:,:,0])
+#plt.show()
+#plt.imshow(x_train[25515,:,:,0])
+#plt.show()
+#plt.imshow(x_train[21215,:,:,0])
+#plt.show()
 
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=VAL_SIZE, shuffle=True)
 
@@ -154,7 +252,7 @@ history = model.fit(x_train, y_train, validation_data=(x_val, y_val),
 loss, acc = model.evaluate(x_test, y_test, verbose=0)
 print(acc * 100)
 
-model.save('2D_subspecies_aug.h5')
+model.save(NAME)
 
 
 
