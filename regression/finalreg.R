10add9368ff3e9a308119623e402c811b49f1498
unknown <janhenrikt>
2020-03-12 18:49:23 +0100

    kleine anpassungen

--- regression/finalreg.R
+++ regression/finalreg.R
@@ -1,43 +1,28 @@
-ahorn <- diameter(dataahorn2)
-roteiche <- diameter(dataroteiche2)
-eiche <- diameter(dataeiche2)
-linde <- diameter(datalinde2)
-hainbuche <- diameter(datahainbuche2)
-esche <- diameter(dataesche2)
-buche <- diameter(databuche2)
-
-douglasie <- diameter(datadouglasie2)
-fichte <- diameter(datafichte2)
-kiefer <- diameter(datakiefer2)
+#skript containing the assembly of the data and the regression
 
 
-#################
+dummy<-rep(1, times = length(dahorn)); ahorn <- cbind(dummy, dahorn)
+dummy<-rep(1, times = length(droteiche)); roteiche <-cbind(dummy, droteiche)
+dummy<-rep(1, times = length(deiche)); eiche <-cbind(dummy, deiche)
+dummy<-rep(1, times = length(dlinde)); linde <-cbind(dummy, dlinde)
+dummy<-rep(1, times = length(dhainbuche)); hainbuche <-cbind(dummy, dhainbuche)
+dummy<-rep(1, times = length(desche)); esche <-cbind(dummy, desche)
+dummy<-rep(1, times = length(dbuche)); buche <-cbind(dummy, dbuche)
 
-dummy<-rep(1, times = length(ahorn[, 1])); ahorn <- cbind(dummy, ahorn)
-dummy<-rep(1, times = length(roteiche[, 1])); roteiche <-cbind(dummy, roteiche)
-dummy<-rep(1, times = length(eiche[, 1])); eiche <-cbind(dummy, eiche)
-dummy<-rep(1, times = length(linde[, 1])); linde <-cbind(dummy, linde)
-dummy<-rep(1, times = length(hainbuche[, 1])); hainbuche <-cbind(dummy, hainbuche)
-dummy<-rep(1, times = length(esche[, 1])); esche <-cbind(dummy, esche)
-dummy<-rep(1, times = length(buche[, 1])); buche <-cbind(dummy, buche)
-
-dummy<-rep(0, times = length(kiefer[, 1])); kiefer <-cbind(dummy, kiefer)
-dummy<-rep(0, times = length(fichte[, 1])); fichte <-cbind(dummy, fichte)
-dummy<-rep(0, times = length(douglasie[, 1])); douglasie <-cbind(dummy, douglasie)
+dummy<-rep(0, times = length(dkiefer)); kiefer <-cbind(dummy, dkiefer)
+dummy<-rep(0, times = length(dfichte)); fichte <-cbind(dummy, dfichte)
+dummy<-rep(0, times = length(ddouglasie)); douglasie <-cbind(dummy, ddouglasie)
 
 
-###### trunkless set
+###### data set
 
 Z <- rbind(kiefer, fichte, douglasie, ahorn, roteiche, eiche, linde, hainbuche, esche, buche)
-
-########################################################################################
-cor(Z[,2], Z[,3])
-Z <- Z[,-2]
-
+colnames(Z) <- c("species","diameter")
+Z <- as.data.frame(Z)
 #########################################################################################
-#glm with full set
+#glm-logit model
 
-myglm1.1<-glm(dummy ~ horizontal, data = Z, family = binomial(link
+myglm1.1<-glm(species ~ diameter, data = Z, family = binomial(link
                                                               ="logit"))
 summary(myglm1.1)
 
@@ -54,9 +39,8 @@ testingset <- Z[a,]
 
 #logit model based on the trainingset
 
-myglm1.2<-glm(dummy ~ horizontal, data = trainingset, family = binomial(link
+myglm1.2<-glm(species ~ diameter, data = trainingset, family = binomial(link
                                                                         ="logit"))
-
 
 # using the trained myglm1.2 model to test it with testingset
 
@@ -65,7 +49,7 @@ prediction <- rep(NA, times = dim(testingset)[1])
 for (i in 1:length(prediction)) {
   
   prediction[i] <- predict(myglm1.2, type = "response", newdata = 
-                             data.frame("horizontal" = testingset[i,2]))
+                             data.frame("diameter" = testingset[i,2]))
 }
 
 
